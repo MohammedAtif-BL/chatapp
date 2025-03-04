a44380482c711a45e66396feb2b74eb6082fc798
MohammedAtif-BL <mohammed.atif@bridgelabz.com>
2025-03-04 16:46:10 +0530

    Added test Cases

--- src/test/java/com/learning/chatapp/chat/ChatApplicationTests.java
+++ src/test/java/com/learning/chatapp/chat/ChatApplicationTests.java
@@ -2,12 +2,14 @@ package com.learning.chatapp.chat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class ChatApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testMain() {
+		assertDoesNotThrow(() -> ChatApplication.main(new String[]{}));
 	}
 
 }
